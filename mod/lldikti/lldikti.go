16eee04f160ff1c29fab68fe210cb119e470bea4
haryadi <haryadiyusuf61@gmail.com>
2024-06-12 14:44:15 +0700

    tambah phone number

--- mod/lldikti/lldikti.go
+++ mod/lldikti/lldikti.go
@@ -4,5 +4,5 @@ import "github.com/whatsauth/itmodel"
 
 func Lldikti(Pesan itmodel.IteungMessage) (reply string) {
 
-	return "Hai.. hai.. ini dia nama alias kaka :\n" + Pesan.Alias_name + "\nsimpan dan catat baik baik ya.. \nmakasih"
+	return "Hai.. hai.. ini dia nama alias kaka:\n" + Pesan.Alias_name + "\nsimpan dan catat baik-baik ya.. \nNomor telepon kaka: " + Pesan.Phone_number + "\nmakasih"
 }
